0b1626d482da166a90d9a9c5e6843647a8d5cecf
kelvinpraises <kelvinpraises@gmail.com>
2022-12-27 00:24:17 +0100

    feat: add labels to show project status

--- src/features/community/views/ProjectLabels.tsx
+++ src/features/community/views/ProjectLabels.tsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import { kMaxContainerWidth } from "../../../common/constants/width";
+import Labels from "../components/Labels";
+
 const ProjectLabels = () => {
-  return <div>ProjectLabels</div>;
+  return (
+    <SProjectLabels>
+      {["voting", "on-chain", "feedback"].map((label) => (
+        <Labels type={label as LabelTypes} />
+      ))}
+    </SProjectLabels>
+  );
 };
 
 export default ProjectLabels;
+
+const SProjectLabels = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin-bottom: 2em;
+  width: 100%;
+  max-width: ${kMaxContainerWidth};
+`;
