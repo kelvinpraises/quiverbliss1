ec9e1c39fdc25c8ce15ef4b15125c56d3aae83d6
kelvinpraises <kelvinpraises@gmail.com>
2022-12-26 20:44:58 +0100

    feat: add connect button

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,5 +1,20 @@
-const Header: React.FC<null> = () => {
-  return <div></div>;
+import styled from "styled-components";
+import ConnectButton from "../../features/authentication/components/ConnectButton";
+import { kMaxContainerWidth } from "../constants/width";
+
+const Header: React.FC = () => {
+  return (
+    <SHeader>
+      <ConnectButton />
+    </SHeader>
+  );
 };
 
 export default Header;
+
+const SHeader = styled.header`
+  justify-content: flex-end;
+  display: flex;
+  width: 100%;
+  max-width: ${kMaxContainerWidth};
+`;
