288b0707a953878c4e1b42e8d63e27fda6b8b075
kelvinpraises <kelvinpraises@gmail.com>
2022-12-26 20:45:30 +0100

    feat: update scaffold to have header and footer

--- src/common/components/Scaffold.tsx
+++ src/common/components/Scaffold.tsx
@@ -1,23 +1,27 @@
 import styled from "styled-components";
+import Footer from "./Footer";
+import Header from "./Header";
 
 interface IScaffoldProp {
   children: React.ReactNode;
+  hide?: string[];
 }
 
-const Scaffold: React.FC<IScaffoldProp> = ({ children }) => {
+const Scaffold: React.FC<IScaffoldProp> = ({ hide, children }) => {
   return (
     <SScaffold>
-      <SContainer>{children}</SContainer>
+      {hide?.includes("header") ? <></> : <Header />}
+      <>{children}</>
+      {hide?.includes("footer") ? <></> : <Footer />}
     </SScaffold>
   );
 };
 
 export default Scaffold;
 
-const SScaffold = styled.div`
+const SScaffold = styled.main`
   display: flex;
-`;
-
-const SContainer = styled.div`
+  flex-direction: column;
+  align-items: center;
   width: 100%;
 `;
