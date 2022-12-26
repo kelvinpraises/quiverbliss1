8a669a7293e295527c95e1fc2e6522078d52d539
kelvinpraises <kelvinpraises@gmail.com>
2022-12-27 00:15:39 +0100

    feat: add figma type as art board

--- src/features/community/components/ArtBoard.tsx
+++ src/features/community/components/ArtBoard.tsx
@@ -1,5 +1,39 @@
-const ArtBoard = () => {
-  return <div>ArtBoard</div>;
+import styled from "styled-components";
+import { kRadiusM } from "../../../common/constants/borderRadius";
+import clampBuilder from "../../../utils/clampBuilder";
+
+const ArtBoard: React.FC<IArtBoard> = ({ type }) => {
+  let artBoard: React.ReactElement;
+
+  const figma = <SFigmaArtBoard></SFigmaArtBoard>;
+
+  switch (type) {
+    case "figma":
+      artBoard = figma;
+      break;
+
+    default:
+      artBoard = <></>;
+      break;
+  }
+
+  return artBoard;
 };
 
 export default ArtBoard;
+
+const SArtBoard = styled.div`
+  border-radius: ${kRadiusM};
+  background-color: ${({ theme }) => theme.card};
+  border: 1.2px solid ${({ theme }) => theme.cardBorder};
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SFigmaArtBoard = styled(SArtBoard)`
+  min-width: ${clampBuilder(320, 703, 20, 35)};
+  height: ${clampBuilder(320, 1200, 13, 11)};
+`;
