3483445358e9aed42f73202f7a3e1e5d50842e5b
kelvinpraises <kelvinpraises@gmail.com>
2022-12-26 21:44:29 +0100

    feat: add supported label types

--- src/features/community/components/Labels.tsx
+++ src/features/community/components/Labels.tsx
@@ -1,5 +1,55 @@
-const Labels = () => {
-  return <div>Labels</div>;
+import styled from "styled-components";
+
+const Labels: React.FC<ILabelsProp> = ({ type }) => {
+  let label: React.ReactElement;
+
+  const voting = (
+    <SLabel color="#00CBD4" backgroundColor="#B2FCFF">
+      VOTING
+    </SLabel>
+  );
+
+  const feedback = (
+    <SLabel color="#00D53E" backgroundColor="#B0FFCC">
+      FEEDBACK
+    </SLabel>
+  );
+
+  const onChain = (
+    <SLabel color="#894DD9" backgroundColor="#C4C2FF">
+      ON-CHAIN
+    </SLabel>
+  );
+
+  switch (type) {
+    case "voting":
+      label = voting;
+      break;
+
+    case "feedback":
+      label = feedback;
+      break;
+
+    case "on-chain":
+      label = onChain;
+      break;
+
+    default:
+      label = <></>;
+      break;
+  }
+
+  return label;
 };
 
 export default Labels;
+
+const SLabel = styled.div<ISLabelProp>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ color }) => color};
+  width: min-content;
+  padding: 0.2em;
+`;
