d2d12f599ccc4ad8539a76acdf7f06810b1688e0
kelvinpraises <kelvinpraises@gmail.com>
2022-12-30 21:24:22 +0100

    feat: init community

---
+++ src/pages/studio/community.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Community() {
+  return (
+    <>
+      <Head>
+        <title>QuiverBliss</title>
+        <meta name="description" content="QuiverBliss" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
