1b4ee5fb8ee2f914990ed83e740e6be03df27b9d
kelvinpraises <kelvinpraises@gmail.com>
2023-01-11 18:05:18 +0100

    feat: add StyledComponentsRegistry

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import StyledComponentsRegistry from './registry';
 import './globals.css'
 
 export default function RootLayout({
@@ -12,7 +13,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
